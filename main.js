3eef24a2b2f5086d1ab5c3d2761bfd66ada4ed94
s3alard <laszlo.szilard2000@gmail.com>
2023-08-13 21:05:24 +0200

    make everything work

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-;
-document.addEventListener('DOMContentLoaded', function () {
-    var addButton = document.getElementById('add-button');
-    addButton.addEventListener('click', function () {
+var addButton = document.getElementById("add-button");
+var dateInput = document.getElementById("date-input");
+var weightInput = document.getElementById("weight-input");
+var currentWeightDisplay = document.getElementById("current-weight-display");
+var startWeightDisplay = document.getElementById("start-weight-display");
+var progressDisplay = document.getElementById("progress-display");
+var weightHistory = document.getElementById("weight-history");
+var existingRecords = JSON.parse(localStorage.getItem("weightData") || "[]");
+document.addEventListener("DOMContentLoaded", function () {
+    addButton.addEventListener("click", function () {
         saveData();
         displayRecords();
     });
+    resort();
+    localStorage.setItem("weightData", JSON.stringify(existingRecords));
     displayRecords();
+    datelimiter();
+    historydisplay();
 });
 function saveData() {
-    var dateInput = document.getElementById("date-input");
-    var weightInput = document.getElementById("weight-input");
     var dateValue = dateInput.value;
     var weightValue = parseFloat(weightInput.value);
     if (dateValue && weightValue) {
@@ -17,24 +25,62 @@ function saveData() {
             date: dateValue,
             weight: weightValue,
         };
-        var existingRecords = JSON.parse(localStorage.getItem('weightData') || '[]');
-        existingRecords.push;
+        existingRecords.push(data);
+        resort();
         localStorage.setItem("weightData", JSON.stringify(existingRecords));
+        historydisplay();
         alert("Success");
     }
     else {
         alert("field empty, try again");
     }
 }
+;
 function displayRecords() {
-    var recordsContainer = document.getElementById('records');
-    var existingRecords = JSON.parse(localStorage.getItem('weightData') || '[]');
-    existingRecords.sort;
-    recordsContainer.innerHTML = '';
-    for (var _i = 0, existingRecords_1 = existingRecords; _i < existingRecords_1.length; _i++) {
-        var record = existingRecords_1[_i];
-        var recordElement = document.createElement('div');
-        recordElement.innerText = "Date: ".concat(record.date, ", Weight: ").concat(record.weight, " kg");
-        recordsContainer.appendChild(recordElement);
+    if (existingRecords.length >= 2) {
+        currentWeightDisplay.innerHTML = existingRecords[existingRecords.length - 1].weight.toString() + " kg";
+        startWeightDisplay.innerHTML = existingRecords[0].weight.toString() + " kg";
+        if (existingRecords[existingRecords.length - 1].weight - existingRecords[0].weight < 0) {
+            progressDisplay.innerHTML = (existingRecords[existingRecords.length - 1].weight - existingRecords[0].weight).toString() + " kg";
+        }
+        else {
+            progressDisplay.innerHTML = '+' + (existingRecords[existingRecords.length - 1].weight - existingRecords[0].weight).toString() + " kg";
+        }
+        ;
+    }
+    ;
+}
+;
+function resort() {
+    existingRecords.sort(function (a, b) {
+        var dateA = new Date(a.date);
+        var dateB = new Date(b.date);
+        if (dateA > dateB) {
+            return 1;
+        }
+        if (dateA < dateB) {
+            return -1;
+        }
+        return 0;
+    });
+}
+;
+function datelimiter() {
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = (today.getMonth() + 1).toString().padStart(2, "0");
+    var day = today.getDate().toString().padStart(2, "0");
+    var maxDate = year + "-" + month + "-" + day;
+    dateInput.max = maxDate;
+}
+;
+function historydisplay() {
+    weightHistory.innerHTML = '';
+    for (var i = existingRecords.length - 1; i > -1 && i > existingRecords.length - 11; i--) {
+        var entry = existingRecords[i];
+        var li = document.createElement('li');
+        li.innerHTML = entry.weight.toString() + ' kg ' + entry.date.toString();
+        weightHistory.appendChild(li);
     }
 }
+;
--- main.ts
+++ main.ts
@@ -1,23 +1,45 @@
+const addButton: HTMLButtonElement = document.getElementById(
+  "add-button"
+) as HTMLButtonElement;
+const dateInput: HTMLInputElement = document.getElementById(
+  "date-input"
+) as HTMLInputElement;
+const weightInput: HTMLInputElement = document.getElementById(
+  "weight-input"
+) as HTMLInputElement;
+const currentWeightDisplay: HTMLDivElement = document.getElementById(
+  "current-weight-display"
+) as HTMLDivElement;
+const startWeightDisplay: HTMLDivElement = document.getElementById(
+  "start-weight-display"
+) as HTMLDivElement;
+const progressDisplay: HTMLDivElement = document.getElementById(
+  "progress-display"
+) as HTMLDivElement;
+const weightHistory: HTMLUListElement = document.getElementById(
+  "weight-history"
+) as HTMLUListElement;
 interface WeightData {
   date: string;
   weight: number;
-};
+}
+let existingRecords: WeightData[] = JSON.parse(
+  localStorage.getItem("weightData") || "[]"
+);
 
-document.addEventListener('DOMContentLoaded', function () {
-  const addButton: HTMLButtonElement = document.getElementById('add-button') as HTMLButtonElement;
-  addButton.addEventListener('click', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  addButton.addEventListener("click", function () {
     saveData();
     displayRecords();
   });
-
+  resort();
+  localStorage.setItem("weightData", JSON.stringify(existingRecords));
   displayRecords();
+  datelimiter();
+  historydisplay();
 });
 
-
 function saveData() {
-  const dateInput: HTMLInputElement = document.getElementById("date-input") as HTMLInputElement;
-  const weightInput: HTMLInputElement = document.getElementById("weight-input") as HTMLInputElement;
-
   const dateValue: string = dateInput.value;
   const weightValue: number = parseFloat(weightInput.value);
 
@@ -26,35 +48,76 @@ function saveData() {
       date: dateValue,
       weight: weightValue,
     };
-    
-
-  const existingRecords: WeightData[] = JSON.parse(localStorage.getItem('weightData') || '[]');
-
-  existingRecords.push;
-
-  localStorage.setItem("weightData", JSON.stringify(existingRecords));
-  alert("Success");
-} else {
-  alert("field empty, try again");
-}
-
-}
-
-function displayRecords() {
-  const recordsContainer: HTMLElement = document.getElementById('records') as HTMLElement;
-
-  const existingRecords: WeightData[] = JSON.parse(localStorage.getItem('weightData') || '[]');
-
-  existingRecords.sort;
-
-  recordsContainer.innerHTML = '';
-
-  for (const record of existingRecords) {
-    const recordElement = document.createElement('div');
-    recordElement.innerText = `Date: ${record.date}, Weight: ${record.weight} kg`;
-    recordsContainer.appendChild(recordElement);
+    existingRecords.push(data);
+    resort();
+    localStorage.setItem("weightData", JSON.stringify(existingRecords));
+    historydisplay();
+    alert("Success");
+  } else {
+    alert("field empty, try again");
   }
 }
 
+function displayRecords() {
+  if (existingRecords.length >= 2) {
+    currentWeightDisplay.innerHTML =
+      existingRecords[existingRecords.length - 1].weight.toString() + " kg";
+    startWeightDisplay.innerHTML = existingRecords[0].weight.toString() + " kg";
+    if (
+      existingRecords[existingRecords.length - 1].weight -
+        existingRecords[0].weight <
+      0
+    ) {
+      progressDisplay.innerHTML =
+        (
+          existingRecords[existingRecords.length - 1].weight -
+          existingRecords[0].weight
+        ).toString() + " kg";
+    } else {
+      progressDisplay.innerHTML =
+        "+" +
+        (
+          existingRecords[existingRecords.length - 1].weight -
+          existingRecords[0].weight
+        ).toString() +
+        " kg";
+    }
+  }
+}
 
+function resort() {
+  existingRecords.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    if (dateA > dateB) {
+      return 1;
+    }
+    if (dateA < dateB) {
+      return -1;
+    }
+    return 0;
+  });
+}
 
+function datelimiter() {
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = (today.getMonth() + 1).toString().padStart(2, "0");
+  var day = today.getDate().toString().padStart(2, "0");
+  const maxDate: string = year + "-" + month + "-" + day;
+  dateInput.max = maxDate;
+}
+
+function historydisplay() {
+  weightHistory.innerHTML = "";
+  for (
+    let i = existingRecords.length - 1;
+    i > -1 && i > existingRecords.length - 11;
+    i--
+  ) {
+    const entry = existingRecords[i];
+    const li = document.createElement("li");
+    li.innerHTML = entry.weight.toString() + " kg " + entry.date.toString();
+    weightHistory.appendChild(li);
+  }
+}
